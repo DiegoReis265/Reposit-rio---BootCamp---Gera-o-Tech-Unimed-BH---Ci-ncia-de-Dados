c02ba3606ff47e404310adafa49427a64c233316
DiegoReis265 <d.felipe66@yahoo.com>
2022-10-03 08:36:15 -0300

    Adicionando o texto dos desafios nos codigos

--- Desafios iniciais de códigos Python/Desafio de código -  Cálculo de viagem.py
+++ Desafios iniciais de códigos Python/Desafio de código -  Cálculo de viagem.py
@@ -1,5 +1,19 @@
 # Desafios iniciais de Python, código 3: Cálculo de viagem
 
+# Desafio
+# Rubens quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem de carro, sendo que seu carro 
+# faz 12 KM/L. Como ele não sabe fazer um programa que o auxilie nessa missão, ele te pede ajuda. Para efetuar o cálculo, 
+# deve-se fornecer o tempo gasto em horas na viagem e a velocidade média durante a mesma em km/h. Assim, você conseguirá 
+# passar para Rubens qual a distância percorrida e, em seguida, calcular quantos litros serão necessários para a viagem que
+# ele quer fazer. Mostre o valor com 3 casas decimais após o ponto.
+
+# Entrada
+# O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem em horas e o segundo é a velocidade média 
+# durante a mesma em km/h.
+
+# Saída
+# Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal
+
 # DICAS SOBRE PYTHON:
 # FUNÇÃO input(): Ela recebe como parâmetro uma String que será visível ao usuário, 
 # onde geralmente informa que tipo de informação ele está esperando receber;
@@ -13,11 +27,13 @@
 # Abaixo segue um exemplo de código que você pode ou não utilizar
 
 valores = input(" Digite a quantidade de horas gastas na viagem e a velocidade media em Km/h: ").split()
-
+# No Site deve ficar  -> valores = input().split()
 tempo_gasto = int(valores[0])
 velocidade_media = int(valores[1])
 
 distancia_percorrida = tempo_gasto * velocidade_media
 litros_necessarios = distancia_percorrida / 12
-print(f' O total de litros necessários para percorrer {distancia_percorrida} Km é: {litros_necessarios:.3f}') 
+print(f' O total de litros necessários para percorrer {distancia_percorrida} Km é: {litros_necessarios:.3f}')
+
+# No site deve ficar  -> print(f' {litros_necessarios:.3f}') 
 # TODO:  Calcule quantidade de litros necessária para realizar a viagem e imprima com TRÊS dígitos após o ponto decimal
--- Desafios iniciais de códigos Python/Desafio de código - Cachorros-Quentes.py
+++ Desafios iniciais de códigos Python/Desafio de código - Cachorros-Quentes.py
@@ -1,5 +1,25 @@
 # Desafios iniciais de Python, código 2: Cachorros-Quentes
 
+# Desafio
+# Em 2012 foi alcançado um novo recorde mundial na famosa Competição de Cachorros-Quentes do Nathan: o campeão, Joey Chestnut,
+# devorou 68 cachorros-quentes em dez minutos, um aumento incrível em relação aos 62 sanduíches devorados pelo mesmo Chestnut em 2011.
+# O restaurante Nathan’s Famous Corporation, localizado no Brooklyn, NY, é o responsável pela competição. Eles produzem deliciosos 
+# cachorros-quentes, mundialmente famosos, mas quando o assunto é matemática eles não são tão bons. Eles desejam ser listados no 
+# Livro de Recordes do Guinness, mas para isso devem preencher um formulário descrevendo os fatos básicos da competição. Em particular, 
+# eles devem informar o número médio de cachorros-quentes consumidos pelos participantes durante a competição. Você pode ajudá-los? 
+# Eles prometeram pagá-lo com um dos seus saborosos cachorros-quentes. Dados o número total de cachorros-quentes consumidos e o número 
+# total de participantes na competição, você deve escrever um programa para determinar o número médio de cachorros-quentes consumidos 
+# pelos participantes.
+
+# Entrada
+# A entrada consiste de uma única linha que contém dois inteiros H e P (1 ≤ H, P ≤ 1000) indicando respectivamente o número total de 
+# cachorros-quentes consumidos e o número total de participantes na competição.
+
+# Saída
+# Seu programa deve produzir uma única linha com um número racional representando o número médio de cachorros-quentes consumidos pelos
+# participantes. O resultado deve ser escrito como um número racional com exatamente dois dígitos após o ponto decimal, arredondado se 
+# necessário.
+
 # DICAS SOBRE PYTHON:
 # FUNÇÃO input(): Ela recebe como parâmetro uma String que será visível ao usuário, 
 # onde geralmente informa que tipo de informação ele está esperando receber;
@@ -13,6 +33,7 @@
 
 # Abaixo segue um exemplo de código que você pode ou não utilizar
 valores = input(" Digite a quantidade de cachorros quentes consumidos e o número de participantes: ").split() 
+# No Site deve ficar  -> entrada = input().split()
 
 total_cachorros_quentes = int (valores[0])
 participantes = int (valores[1])
@@ -20,7 +41,6 @@ if(total_cachorros_quentes >=1 and participantes <= 1000 ):
   media_cachorros_quentes = total_cachorros_quentes / participantes
   print (f' O valor médio de cachorros quentes consumidos por participante é: {media_cachorros_quentes:.2f}')
 
-# TODO:  Calcule a média de cachorros quentes consumidas por participante e imprima o valor com DUAS casas decimais.
 # No site deve ficar  -> print(f' {media_cachorros_quentes:.2f}')
-# Para arredondamento das casas decimais poderia ter sido utilizada a função round(),
-# ficaria print (f' O valor médio de cachorros quentes consumidos por participante é: {round(media_cachorros_quentes, 2)}') 
+# TODO:  Calcule a média de cachorros quentes consumidas por participante e imprima o valor com DUAS casas decimais.
+
